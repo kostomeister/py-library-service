a41080a74f556e8770872e50556fb63fd8d9758b
Kostia <kostia3507@gmail.com>
2023-12-14 12:07:13 +0200

    added urls for managing authentication

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
-    path("", include("")),
+    path("", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
+
 
 app_name = "users"
