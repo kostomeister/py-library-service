5db80d1d218e07d2e8d771948e68ad4b61d1d72b
Kostia <kostia3507@gmail.com>
2023-12-14 12:06:51 +0200

    added view for managing user

--- user/views.py
+++ user/views.py
@@ -1,6 +1,15 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-from .serializers import UserSerializer
+from user.serializers import UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
 
 
 class CreateUserView(generics.CreateAPIView):
