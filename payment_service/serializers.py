270d1a254b3d8d4cee028ed7a4c6d25bafbad184
Kostia <kostia3507@gmail.com>
2023-12-14 18:19:57 +0200

    added serailizer for payment model

---
+++ payment_service/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from payment_service.models import Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "status",
+            "type",
+            "borrowing",
+            "session_url",
+            "session_id",
+            "money_to_pay"
+        )
