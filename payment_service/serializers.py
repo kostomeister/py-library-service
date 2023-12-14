dbb7c9cf2c7d5a1ed4fd94812ae5c2fd2913f72d
Kostia <kostia3507@gmail.com>
2023-12-14 18:56:59 +0200

    added detail serializer for payment model

--- payment_service/serializers.py
+++ payment_service/serializers.py
@@ -29,3 +29,24 @@ class PaymentListSerializer(PaymentSerializer):
             "borrowing",
             "money_to_pay"
         )
+
+
+class PaymentDetailSerializer(PaymentSerializer):
+    book = serializers.CharField(
+        source="borrowing.book_id.title", read_only=True
+    )
+    return_date = serializers.CharField(
+        source="borrowing.expected_return_date", read_only=True
+    )
+
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "status",
+            "return_date",
+            "book",
+            "money_to_pay",
+            "session_url",
+            "session_id"
+        )
