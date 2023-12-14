3702dd2acc3728ddf8749c29247139205a06cb8e
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-12-14 14:09:08 +0200

    Fixed BookDetailSerializer

--- book_service/serializers.py
+++ book_service/serializers.py
@@ -5,16 +5,14 @@ from book_service.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = "__all__"
+        fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
 
 
 class BookListSerializer(BookSerializer):
     class Meta:
         model = Book
-        fields = ["id", "title", "author", "inventory", "daily_fee"]
+        fields = ("id", "title", "author", "inventory", "daily_fee")
 
 
 class BookDetailSerializer(BookSerializer):
-    class Meta:
-        model = Book
-        fields = "__all__"
+    pass
