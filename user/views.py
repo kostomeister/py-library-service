23ae09ff0621b0605ceadee25e61f8db2f33b545
Kostia <kostia3507@gmail.com>
2023-12-14 11:59:55 +0200

    added view for creating user

--- user/views.py
+++ user/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
