aa098bbef01b8b3671b02c6681f9e3004c684a4a
Kostia <kostia3507@gmail.com>
2023-12-14 12:02:44 +0200

    wrote todo commentary

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -131,6 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "user.User"
 
+# TODO: add confirmation by email
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
