edc25b0400ff0a9e5e18b19425b52075c50e32df
Ruslan Misko <miskoruslan6@gmail.com>
2023-12-15 15:24:58 +0200

    fix return type from float to int and * 100 in cents

--- payment_service/calculation_of_the_amount_to_be_paid.py
+++ payment_service/calculation_of_the_amount_to_be_paid.py
@@ -7,11 +7,11 @@ def calculating_total_sum_for_begin_of_borrowing(
     daily_fee: float, expected_return: datetime
 ) -> float:
     days_in_borrowing = expected_return - datetime.date.today()
-    return round((days_in_borrowing.days + 1) * daily_fee, 2)
+    return int((days_in_borrowing.days + 1) * daily_fee * 100)
 
 
 def calculating_sum_of_fine(
     daily_fee: float, expected_return: datetime, actual_return: datetime
 ) -> float:
     fine_days = actual_return - expected_return
-    return round(fine_days.days * daily_fee * FINE_MULTIPLIER, 2)
+    return int(fine_days.days * daily_fee * FINE_MULTIPLIER * 100)
