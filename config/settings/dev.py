7de3d1263fe1064db3fb144376cd99ae28dc4cb5
Ruslan Misko <miskoruslan6@gmail.com>
2023-12-15 16:59:54 +0200

    Change the pre-name for jwt authorization in ModHeader from Authorization to Auth

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -143,4 +143,6 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "Authorize",
 }
