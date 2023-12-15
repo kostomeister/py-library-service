3f4b682e76379084a0a987f60d813212a549514a
vshvanska <ladyvaleriya13@gmail.com>
2023-12-15 15:51:45 +0200

    Created notify overdue borrowing function

--- notifications/messages.py
+++ notifications/messages.py
@@ -1,6 +1,8 @@
 import os
 import requests
 
+from notifications.models import Notification
+
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 
@@ -9,3 +11,10 @@ def send_message(chat_id, notification_text):
         f"https://api.telegram.org/bot{BOT_TOKEN}"
         f"/sendMessage?chat_id={chat_id}"
         f"&text={notification_text}")
+
+
+def notify_overdue_borrowing(user_id):
+    notification = Notification.objects.get(user_id=user_id)
+    text = (f"Hi, {notification.telegram_username} your borrowing is overdue. "
+            f"Please return it as soon as possible.")
+    return send_message(notification.chat_id, text)
