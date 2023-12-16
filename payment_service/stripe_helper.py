f17a92f80b734e3dc334bf15b4604450bc4fd66c
Ruslan Misko <miskoruslan6@gmail.com>
2023-12-16 05:17:23 +0200

    implement function for create fine session

--- payment_service/stripe_helper.py
+++ payment_service/stripe_helper.py
@@ -39,37 +39,36 @@ def create_initial_session(borrowing, request):
     )
     return session
 
-# TODO implement fine stripe session
 
-#
-# def create_fine_session(borrowing, request):
-#     session = stripe.checkout.Session.create(
-#         payment_method_types=['card'],
-#         line_items=[{
-#             'price_data': {
-#                 'currency': 'usd',
-#                 'product_data': {
-#                     'name': "Fine for overdue of " + borrowing.book_id.title,
-#                 },
-#                 'unit_amount': calculating_total_sum_for_begin_of_borrowing(
-#                     borrowing.book_id.daily_fee,
-#                     borrowing.expected_return_date
-#                 )
-#             },
-#             'quantity': 1,
-#         }],
-#         mode='payment',
-#         success_url=request.build_absolute_uri(
-#             reverse(
-#                 "payments:payment-success",
-#                 kwargs={"borrowing_id": borrowing.id}
-#             )
-#         ),
-#         cancel_url=request.build_absolute_uri(
-#             reverse(
-#                 "payments:payment-cancel",
-#                 kwargs={"borrowing_id": borrowing.id}
-#             )
-#         )
-#     )
-#     return session
+def create_fine_session(borrowing, request):
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[{
+            "price_data": {
+                "currency": "usd",
+                "product_data": {
+                    "name": "Fine for overdue of " + borrowing.book_id.title,
+                },
+                "unit_amount": calculating_sum_of_fine(
+                    borrowing.book_id.daily_fee,
+                    borrowing.expected_return_date,
+                    borrowing.actual_return,
+                )
+            },
+            "quantity": 1,
+        }],
+        mode="payment",
+        success_url=request.build_absolute_uri(
+            reverse(
+                "payments:payment-success-fine",
+                kwargs={"borrowing_id": borrowing.id}
+            )
+        ),
+        cancel_url=request.build_absolute_uri(
+            reverse(
+                "payments:payment-cancel",
+                kwargs={"borrowing_id": borrowing.id}
+            )
+        )
+    )
+    return session
