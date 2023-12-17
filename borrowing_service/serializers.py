801e00e7d13aad3c583f5c2ec1ccd7e950079a4b
vshvanska <ladyvaleriya13@gmail.com>
2023-12-17 19:19:51 +0200

    Added payment in borrowingdeail

--- borrowing_service/serializers.py
+++ borrowing_service/serializers.py
@@ -12,6 +12,7 @@ from book_service.models import Book
 from book_service.serializers import BookSerializer
 from borrowing_service.models import Borrowing
 from payment_service.models import Payment
+from payment_service.serializers import PaymentListSerializer
 from payment_service.stripe_helper import create_initial_session
 from user.serializers import UserSerializer
 
@@ -81,6 +82,7 @@ class BorrowingListSerializer(serializers.ModelSerializer):
 class BorrowingDetailSerializer(serializers.ModelSerializer):
     user = UserSerializer(source="user_id", read_only=True)
     book = BookSerializer(source="book_id", read_only=True)
+    payments = PaymentListSerializer(many=True, read_only=True)
 
     class Meta:
         model = Borrowing
@@ -91,6 +93,7 @@ class BorrowingDetailSerializer(serializers.ModelSerializer):
             "actual_return",
             "book",
             "user",
+            "payments"
         )
 
 
