7d40f31c496852cb6b6fdccb2658b6112df7a5cc
Kostia <kostia3507@gmail.com>
2023-12-17 19:41:18 +0200

    added notify function for expired sessions

--- notifications/messages.py
+++ notifications/messages.py
@@ -31,3 +31,29 @@ def notify_overdue_borrowing(user_id):
         f"Please return it as soon as possible."
     )
     return send_message(notification.chat_id, text)
+
+
+def notify_invalid_session(user_id):
+    """
+    Notifies a user about an invalid Stripe session via Telegram.
+
+    Retrieves the user's id and sends a notification message about the invalid session.
+
+    Args:
+    - user_id (int): The ID of the user.
+
+    Returns:
+    - Response: Response object from the send_message function call.
+    """
+    print(user_id)
+    try:
+        notification = Notification.objects.get(user_id=user_id)
+        text = (f"Hi, {notification.telegram_username}! 😕\n\n"
+                f"We're sorry, but it seems that your session is expired. "
+                f"Please review and try again or contact our support for assistance.")
+        return send_message(
+            chat_id=notification.chat_id,
+            notification_text=text
+        )
+    except Notification.DoesNotExist:
+        print("This user is not registered in telegram")
