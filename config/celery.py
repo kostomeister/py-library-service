00acc86727e78b52b4f1c8c967703a917ecd2808
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-12-18 00:50:52 +0200

    Implemented docker for library

--- config/celery.py
+++ config/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
-app = Celery("proj")
+app = Celery("config")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
